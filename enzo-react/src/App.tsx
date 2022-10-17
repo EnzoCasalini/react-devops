ba2d87868dd845475c58aee0cbd05711b86cb002
EnzoCasalini <enzocasa83@gmail.com>
2022-10-17 16:44:46 +0200

    Passage en TSX

---
+++ enzo-react/src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://images-ext-2.discordapp.net/external/E98-NSWgiDpQVkYUzKmWpbHGtqPAYf0L82kYpdrEmhY/https/images-platform.99static.com/v63Qlzd0xc1iJrvvEwQeVHnlkdM%3D/0x101%3A1000x1101/500x500/top/smart/99designs-contests-attachments/96/96452/attachment_96452667" className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
