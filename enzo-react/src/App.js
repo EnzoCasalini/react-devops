5087ccd080a5b125c3e83722be22f70bcf5e0029
EnzoCasalini <enzocasa83@gmail.com>
2022-10-17 16:01:16 +0200

    Test port 3006

--- enzo-react/src/App.js
+++ enzo-react/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://images-ext-2.discordapp.net/external/E98-NSWgiDpQVkYUzKmWpbHGtqPAYf0L82kYpdrEmhY/https/images-platform.99static.com/v63Qlzd0xc1iJrvvEwQeVHnlkdM%3D/0x101%3A1000x1101/500x500/top/smart/99designs-contests-attachments/96/96452/attachment_96452667"className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Yeeeehaaaaaa !!
         </a>
       </header>
     </div>
