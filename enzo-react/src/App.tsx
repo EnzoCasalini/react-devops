0e88ef18c6071eb0b0f04a3e5881ba3b8044e06a
EnzoCasalini <enzocasa83@gmail.com>
2022-11-14 12:04:34 +0100

    Là c'est vraiment fonctionnel

--- enzo-react/src/App.tsx
+++ enzo-react/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Là, ça marche !
+          S/o le gang!
         </a>
       </header>
     </div>
